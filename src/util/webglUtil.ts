8f02258a494e7183fd3675ece3188ba2b56e96c9
kenbunroku <kenbunroku524@gmail.com>
2023-11-30 09:31:59 -0600

    fix: logic to create framebuffer

--- src/util/webglUtil.ts
+++ src/util/webglUtil.ts
@@ -75,54 +75,30 @@ export const createProgram = async (
   return program;
 };
 
-export const createTexture = (
-  gl: WebGL2RenderingContext,
-  width: number,
-  height: number
-): WebGLTexture => {
-  const texture = gl.createTexture();
-  if (!texture) {
-    throw new Error("Unable to create a texture.");
-  }
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.texImage2D(
-    gl.TEXTURE_2D,
-    0, // level
-    gl.RGBA, // internalFormat
-    width, // width
-    height, // height
-    0, // border
-    gl.RGBA, // format
-    gl.UNSIGNED_BYTE, // type
-    null // data
-  );
-  gl.generateMipmap(gl.TEXTURE_2D);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.bindTexture(gl.TEXTURE_2D, null);
-
-  return texture;
-};
-
 export const createFramebuffer = (
   gl: WebGL2RenderingContext,
   width: number,
-  height: number,
-  texture: WebGLTexture
-) => {
+  height: number
+): { framebuffer: WebGLFramebuffer; texture: WebGLTexture } => {
   const framebuffer = gl.createFramebuffer();
   gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
-  const depthBuffer = gl.createRenderbuffer();
-  gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer);
-  gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, width, height);
-  gl.framebufferRenderbuffer(
-    gl.FRAMEBUFFER,
-    gl.DEPTH_ATTACHMENT,
-    gl.RENDERBUFFER,
-    depthBuffer
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    width,
+    height,
+    0,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    null
   );
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
   gl.framebufferTexture2D(
     gl.FRAMEBUFFER,
     gl.COLOR_ATTACHMENT0,
@@ -130,86 +106,12 @@ export const createFramebuffer = (
     texture,
     0
   );
-  gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+  gl.bindTexture(gl.TEXTURE_2D, null);
   gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 
-  return framebuffer;
-};
-
-type UniformType =
-  | "uniform1f"
-  | "uniform2f"
-  | "uniform3f"
-  | "uniform4f"
-  | "uniform1i"
-  | "uniform2i"
-  | "uniform3i"
-  | "uniform4i"
-  | "uniform1ui"
-  | "uniform2ui"
-  | "uniform3ui"
-  | "uniform4ui"
-  | "uniform1fv"
-  | "uniform2fv"
-  | "uniform3fv"
-  | "uniform4fv"
-  | "uniform1iv"
-  | "uniform2iv"
-  | "uniform3iv"
-  | "uniform4iv"
-  | "uniform1uiv"
-  | "uniform2uiv"
-  | "uniform3uiv"
-  | "uniform4uiv"
-  | "uniformMatrix2fv"
-  | "uniformMatrix3fv"
-  | "uniformMatrix4fv";
-
-export const setUniform = (
-  gl: WebGL2RenderingContext,
-  program: WebGLProgram,
-  uniformName: string,
-  type: UniformType,
-  ...values: any[]
-) => {
-  const location = gl.getUniformLocation(program, uniformName);
-  if (location === null) {
-    console.warn(`Uniform '${uniformName}' not found.`);
-    return;
+  if (framebuffer === null || texture === null) {
+    throw new Error("Unable to create a framebuffer.");
   }
 
-  switch (type) {
-    case "uniform1f":
-    case "uniform1i":
-    case "uniform1ui":
-      gl[type](location, values[0]);
-      break;
-
-    case "uniform2f":
-    case "uniform2i":
-    case "uniform2ui":
-      gl[type](location, values[0], values[1]);
-      break;
-
-    case "uniform1fv":
-    case "uniform1iv":
-    case "uniform1uiv":
-      if (values.length === 1) {
-        gl[type](location, values[0]);
-      } else if (values.length === 2) {
-        gl[type](location, values[0], values[1]);
-      } else if (values.length === 3) {
-        gl[type](location, values[0], values[1], values[2]);
-      }
-      break;
-
-    case "uniformMatrix2fv":
-    case "uniformMatrix3fv":
-    case "uniformMatrix4fv":
-      gl[type](location, false, values[0]); // Assuming matrices are not transposed
-      break;
-
-    default:
-      console.warn(`Unknown uniform type: '${type}'`);
-  }
+  return { framebuffer: framebuffer, texture: texture };
 };
