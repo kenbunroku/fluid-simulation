c8bc050caf926cdd0391566214937c897dd93a5d
kenbunroku <kenbunroku524@gmail.com>
2023-11-30 09:31:10 -0600

    fix: logic to calculate center and force

--- src/util/mouse.ts
+++ src/util/mouse.ts
@@ -1,35 +1,63 @@
 import { vec2 } from "./math";
 
+interface MouseProps {
+  mouse_force: number;
+  cursor_size: number;
+  cellScale: {
+    x: number;
+    y: number;
+  };
+}
+
 export const mouseState = {
   center: vec2(0, 0),
   force: vec2(0, 0),
 };
 
-let previousMouseX = 0;
-let previousMouseY = 0;
+let previousMouseCoords = vec2(0, 0);
+let mouseMoved = false;
 
-export const initMouseEvents = (canvas: HTMLCanvasElement | null) => {
+export const initMouseEvents = (
+  canvas: HTMLCanvasElement | null,
+  props: MouseProps
+) => {
   if (!canvas) return;
+
+  const onMouseMove = (event: MouseEvent) => {
+    const target = event.target as HTMLCanvasElement;
+    if (!target) return;
+
+    // Convert mouse position to normalized device coordinates
+    const rect = target.getBoundingClientRect();
+    const mouseX = (event.clientX / rect.width) * 2 - 1;
+    const mouseY = -(event.clientY / rect.height) * 2 + 1;
+
+    // Update mouse state
+    mouseState.center = vec2(mouseX, mouseY);
+    mouseMoved = true;
+    updateMouseState();
+  };
+
   canvas.addEventListener("mousemove", onMouseMove, false);
-};
 
-const onMouseMove = (event: MouseEvent) => {
-  const target = event.target as HTMLCanvasElement;
-  if (!target) return;
+  const updateMouseState = () => {
+    if (!mouseMoved) {
+      mouseState.force = vec2(0, 0);
+      return;
+    }
 
-  // Convert mouse position to normalized device coordinates
-  const rect = target.getBoundingClientRect();
-  const mouseX = (event.clientX / rect.width) * 2 - 1;
-  const mouseY = (event.clientY / rect.height) * 2 + 1;
+    // Calculate force
+    const forceX =
+      ((mouseState.center.x - previousMouseCoords.x) / 2) * props.mouse_force;
+    const forceY =
+      ((mouseState.center.y - previousMouseCoords.y) / 2) * props.mouse_force;
+    mouseState.force = vec2(forceX, forceY);
 
-  // calculate force based on mouse position
-  const forceX = mouseX - previousMouseX;
-  const forceY = mouseY - previousMouseY;
+    // Update previous mouse position
+    previousMouseCoords = mouseState.center;
 
-  // update previous mouse position
-  previousMouseX = mouseX;
-  previousMouseY = mouseY;
+    mouseMoved = false;
+  };
 
-  mouseState.center = vec2(mouseX, mouseY);
-  mouseState.force = vec2(forceX, forceY);
+  return updateMouseState;
 };
